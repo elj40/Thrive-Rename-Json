31c0f848532fd3be29de4aa0770c831f525fb3e6
elj40 <elaijoubert@gmail.com>
2023-10-24 17:20:05 +0200

    Working without arrays

--- thrive_rename_json.py
+++ thrive_rename_json.py
@@ -1,19 +1,36 @@
-import json;
+import json
+import sys
 
-def printKeys(indent, d):
-    for key in d:
-        print("\t"*indent + key + "\t" + str(type(d[key])))
-        if type(d[key]) is dict:
-            #print("")
-            printKeys(indent + 1, d[key])
+def PascalCasify(keyName):
+    newName = keyName[0].upper() + keyName[1:]
+    return newName
 
-data = open(r'C:\Users\elaij\OneDrive\Documents\Programming\Python\programs\Thrive\test_data.json', "r").read()
-jsonData = json.loads(data)
+
+def convertJson(indent, baseDict, newDict):
+    for key in baseDict:
+        #print("\t"*indent + key + "\t" + str(type(d[key])))
+        if type(baseDict[key]) is dict:
+            newKey = PascalCasify(key) if indent > 0 and len(key)>1 else key
+            newDict[newKey] = {}
+            convertJson(indent + 1, baseDict[key], newDict[newKey])
+        else:
+            newDict[PascalCasify(key) if indent > 0  and len(key)>1 else key] = baseDict[key]
+        
+print(sys.argv[1])
+
+PATH_TO_FILE = sys.argv[1] + "\\" + sys.argv[2]
+FILE = open(PATH_TO_FILE)
+DATA = FILE.read()
+
+#data = open(r'C:\Users\elaij\OneDrive\Documents\Programming\Python\programs\Thrive\test_data.json', "r").read()
+jsonData = json.loads(DATA)
 jsonConverted = {}
 
 
 convertedDataFile = open(r'C:\Users\elaij\OneDrive\Documents\Programming\Python\programs\Thrive\converted.json', "w")
 
-printKeys(0, jsonData)
+convertJson(0, jsonData, jsonConverted)
 
-#print(jsonData)
+jsonConvertedText = json.dumps(jsonConverted, indent=4)
+convertedDataFile.write(jsonConvertedText)
+print(jsonConverted)
