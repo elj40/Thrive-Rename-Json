4585226375708d7b9746a61f814d1ce5264e5058
elj40 <elaijoubert@gmail.com>
2023-10-30 14:30:27 +0200

    Ignoring cross-referenced variables

--- thrive_rename_json.py
+++ thrive_rename_json.py
@@ -11,36 +11,57 @@ def PascalCasify(keyName):
     newName = keyName[0].upper() + keyName[1:]
     return newName
 
+def find_ignore_dir(path, ignore_dict):
+    for (root, dirs, files) in os.walk(path):
+        for f in files:
+            if f.find(".json") > 0:
+                find_ignore_file(os.path.join(root,f), ignore_dict)
+        for d in dirs:
+            find_ignore_dir(os.path.join(root,d), ignore_dict)
 
-def convertJson(indent, baseDict):
+def find_ignore_file(path, ignore_dict):
+    FILE = open(path, encoding='utf-8-sig')
+    data = FILE.read()
+    json_data = json.loads(data)
+    FILE.close()
+    for key in json_data:
+        ignore_dict[key] = True
+
+def convertJson(indent, baseDict, ignore):
     newDict = {}
 
     for key in baseDict:
-        newKey = PascalCasify(key) if indent > 0  and len(key)>1 else key
+        newKey = PascalCasify(key) if len(key)>1  and not key in ignore else key
         if type(baseDict[key]) is dict:
-            newDict[newKey] = convertJson(indent + 1, baseDict[key])
+            newDict[newKey] = convertJson(indent + 1, baseDict[key], ignore)
         elif type(baseDict[key]) is list:
             newDict[newKey] = []
             for i in range(len(baseDict[key])):
                 if type(baseDict[key][i]) is dict:
-                    newDict[newKey][i] = convertJson(indent + 1, baseDict[key][i])
+                    newDict[newKey].append(convertJson(indent + 1, baseDict[key][i], ignore))
                 else:
-                    newDict[newKey][i] = baseDict[key][i]
+                    newDict[newKey].append(baseDict[key][i])
         else:
             newDict[newKey] = baseDict[key]
 
     return newDict
 
-def process_dir(path):
-    print("Dir does nothing yet")
+def process_dir(path, ignore):
+    for (root, dirs, files) in os.walk(path):
+        for f in files:
+            if f.find(".json") > 0:
+                print(f)
+                process_file(os.path.join(root,f), ignore)
+        for d in dirs:
+            process_dir(os.path.join(root, d), ignore)
 
-def process_file(path):
+def process_file(path, ignore):
     FILE = open(path, "r", encoding='utf-8-sig')
     DATA = FILE.read()
     FILE.close()
     JSON_DATA = json.loads(DATA)
 
-    jsonConverted = convertJson(0, JSON_DATA)
+    jsonConverted = convertJson(0, JSON_DATA, ignore)
     
     jsonConvertedText = json.dumps(jsonConverted, indent=2)
 
@@ -54,12 +75,18 @@ def main():
         print_help()
         sys.exit()
 
-    print(sys.argv[1] + "\\" + sys.argv[2])
-    PATH_TO_FILE = sys.argv[1] + "\\" + sys.argv[2]
+    IGNORE = {}
+    PATH_TO_FILE = '\\'.join(sys.argv[1:])
+
     if os.path.isdir(PATH_TO_FILE):
-        process_dir(PATH_TO_FILE)
+        find_ignore_dir(PATH_TO_FILE, IGNORE)
+        process_dir(PATH_TO_FILE, IGNORE)
     elif os.path.isfile(PATH_TO_FILE):
-        process_file(PATH_TO_FILE)
+        find_ignore_file(PATH_TO_FILE, IGNORE)
+        print(IGNORE)
+        process_file(PATH_TO_FILE, IGNORE)
+    else:
+        print("Could not find file...")
     
 
 
